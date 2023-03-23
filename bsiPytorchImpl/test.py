13401e57b2aa328a7899885af9f15124ac3ba917
Sachin Pothukuchi <sachin.pothukuchi@gmail.com>
2023-03-23 13:31:49 +0530

    updated file structure

--- bsiPytorchImpl/test.py
+++
@@ -1,7 +0,0 @@
-import torch
-import bsi_ops
-
-print('import works')
-m = torch.tensor([1,2,3,4], dtype=torch.int64)
-n = torch.tensor([5,6,7,8], dtype=torch.int64)
-print(bsi_ops.dot_product(m, n))
