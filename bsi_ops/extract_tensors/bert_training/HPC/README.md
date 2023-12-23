6adf29a06300aae4082be54bfa576912ee913db0
akanksha joshi <akanksha276@gmail.com>
2023-12-22 20:34:01 -0800

    Added HPC training scripts

---
+++ bsi_ops/extract_tensors/bert_training/HPC/README.md
@@ -0,0 +1,32 @@
+### Create a python virtual environment
+
+python3 -m venv venv
+
+### Activate virtual environment to install requirements
+
+source venv/bin/activate 
+
+### Install Requirements 
+
+##### TORCH with CUDA 
+
+`pip3 install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0 --extra-index-url https://download.pytorch.org/whl/cu113`
+
+##### requirements.txt file
+
+pip3 install -r requirements.txt
+
+### Install the dataset
+wget -c https://cdn-datasets.huggingface.co/EsperBERTo/data/oscar.eo.txt
+
+### Create folder to store outputs
+mkdir EsperBERTo
+
+### Run Bash script
+sbatch train_bash.sh
+
+### To check status in queue
+squeue
+squeue -u <user_id>
+
+
---
+++ bsi_ops/extract_tensors/bert_training/HPC/train_bash.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --mail-user=akankshapankaj.joshi@sjsu.edu
+#SBATCH --mail-user=/dev/null
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --job-name=bertTrain
+#SBATCH --output=bertTrain_%j.out
+#SBATCH --error=bertTrain_%j.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=48:00:00
+##SBATCH --mem-per-cpu=2000
+##SBATCH --gres=gpu:p100:1
+#SBATCH --partition=gpu
+
+# on coe-hpc1 cluster load
+module load python3/3.8.8
+#
+# on coe-hpc2 cluster load:
+# module load python-3.10.8-gcc-11.2.0-c5b5yhp slurm
+
+export http_proxy=http://172.16.1.2:3128; export https_proxy=http://172.16.1.2:3128
+
+cd /home/016690830/RA
+source /home/016690830/venv/bin/activate
+
+#nvidia-smi
+
+python3 train.py
