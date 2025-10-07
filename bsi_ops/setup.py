f741406a324bd389aaba28fd9765a023c2ee98de
poornaravuri1108 <poornaravuri4420@gmail.com>
2025-10-07 15:06:31 -0700

    fix to fix so import error

--- bsi_ops/setup.py
+++ bsi_ops/setup.py
@@ -20,7 +20,9 @@ cuda_sources = cpu_sources + [
     'csrc/cuda/bsi_cuda_kernels.cu',
 ]
 
+torch_lib_dir = os.path.join(os.path.dirname(torch.__file__), 'lib')
 common_cxx_flags = ['-std=c++20', '-O3']
+common_link_args = [f'-Wl,-rpath,{torch_lib_dir}']
 
 if has_cuda():
     ext = CUDAExtension(
@@ -30,12 +32,14 @@ if has_cuda():
             'cxx': common_cxx_flags + ['-DBSI_WITH_CUDA=1'],
             'nvcc': ['-O3', '-std=c++20']
         },
+        extra_link_args=common_link_args,
     )
 else:
     ext = CppExtension(
         name='bsi_ops',
         sources=cpu_sources,
         extra_compile_args=common_cxx_flags,
+        extra_link_args=common_link_args,
     )
 
 setup(
