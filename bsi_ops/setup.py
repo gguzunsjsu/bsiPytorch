3bd66665922d5ee3aaf48fba8b283c8c71217850
poornaravuri1108 <poornaravuri4420@gmail.com>
2025-10-11 11:06:35 -0700

    fixing setup.py

--- bsi_ops/setup.py
+++ bsi_ops/setup.py
@@ -11,12 +11,15 @@ def has_cuda():
         return False
 
 
+# CPU-only sources (no CUDA wrapper)
 cpu_sources = [
     'bsiFunctions.cpp',
-    'csrc/cuda/bsi_cuda.cpp',
 ]
 
-cuda_sources = cpu_sources + [
+# CUDA sources include the CUDA wrapper
+cuda_sources = [
+    'bsiFunctions.cpp',
+    'csrc/cuda/bsi_cuda.cpp',
     'csrc/cuda/bsi_cuda_kernels.cu',
 ]
 
@@ -24,13 +27,17 @@ torch_lib_dir = os.path.join(os.path.dirname(torch.__file__), 'lib')
 common_cxx_flags = ['-std=c++20', '-O3']
 common_link_args = [f'-Wl,-rpath,{torch_lib_dir}']
 
+# Add bsiCPP include path
+bsicpp_include = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'bsiCPP'))
+
 if has_cuda():
     ext = CUDAExtension(
         name='bsi_ops',
         sources=cuda_sources,
+        include_dirs=[bsicpp_include],
         extra_compile_args={
             'cxx': common_cxx_flags + ['-DBSI_WITH_CUDA=1'],
-            'nvcc': ['-O3', '-std=c++20']
+            'nvcc': ['-O3', '-std=c++20', '-DBSI_WITH_CUDA=1']
         },
         extra_link_args=common_link_args,
     )
@@ -38,6 +45,7 @@ else:
     ext = CppExtension(
         name='bsi_ops',
         sources=cpu_sources,
+        include_dirs=[bsicpp_include],
         extra_compile_args=common_cxx_flags,
         extra_link_args=common_link_args,
     )
